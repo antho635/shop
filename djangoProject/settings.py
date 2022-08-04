c84c5d5cc3b306ca445c9b3272ae6c56814b0d56
Toutchy kipik <toutchy.kipik@pm.me>
2022-08-04 09:10:07 +0200

    collectstatic

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -128,5 +129,4 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 AUTH_USER_MODEL = 'accounts.Shopper'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 django_heroku.settings(locals())
