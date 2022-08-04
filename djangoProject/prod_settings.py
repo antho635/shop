402b937e1a28fb118675576e6b6b490dce8d2362
Toutchy kipik <toutchy.kipik@pm.me>
2022-08-04 10:18:50 +0200

    allé

---
+++ djangoProject/prod_settings.py
@@ -0,0 +1,9 @@
+from .settings import *
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+SECRET_KEY = 'r_%o_05r@xod5w#2dh5hx74%aqen9g41&q+f&wx7nu8hqjs*r*'
+
+ALLOWED_HOSTS = ['https://mon-commerce.herokuapp.com']
